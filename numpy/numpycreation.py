fc6def1a5a28c924f310c11d47a98c49f203c0c1
Nagabandaru2003 <nagasarojabandaru@gmail.com>
2025-03-20 23:11:14 +0530

    done

---
+++ numpy/numpycreation.py
@@ -0,0 +1,7 @@
+import numpy as np
+arr = np.array([1,2,3])
+print('array1: ',arr)
+arr = np.array([[1,2,3],[4,5,6]])
+print('array2: ',arr)
+arr = np.array((2,4,6))
+print('tuple: ',arr)
---
+++ numpy/oneD.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+list = [1,2,3,4]
+
+print("list",list)
+
+s_arr = np.array(list)
+print("oneD array: ",s_arr)
+
+print(type(list))
+print(type(s_arr))
